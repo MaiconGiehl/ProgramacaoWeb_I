e70562923b23537fae982f8578f097c7e42f008e
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-03-15 15:14:50 -0300

    first commit

---
+++ 2022-03-21/FORM/back.js
@@ -0,0 +1,9 @@
+function submit () {
+
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let cidadeNatal = document.getElementById("cidadeNatal").value;
+    let cpf = document.getElementById("cpf").value;
+    
+    console.log(nome, idade, cidadeNatal, cpf)
+}
